7b024c9c9efc73e964cfef777dc6b3821e05eedd
Gabriel Delfino <gabrieldelfino28@gmail.com>
2019-08-03 13:12:25 -0300

    Java

--- src/model/App.java
+++ src/model/App.java
@@ -5,23 +5,28 @@ import java.text.DecimalFormat;
 
 public class App {
 	
+	public static double fahrenheit(double f) { //Método do calculo de converter fahrenheit em celsius
+		return (f-32)*5/9;
+	}
+	
+	public static double kelvin(double k) {//Método do calculo de converter kelvin em celcius
+		return k-273.15;
+	}
 	
 	public static void main(String[] args) {
 		DecimalFormat df = new DecimalFormat("###,##0.00");
-		double c;
-		double b;
 		
 		double f = Double.parseDouble(JOptionPane.showInputDialog(null, "\nConversor de Fahrenheit para Celsius\nDigite o valor de Fahrenheit: "));
-		c=(f-32)*5/9;
+
 		
-		JOptionPane.showMessageDialog(null, "Valor Convertido: " + c+"ºCelsius");
-		System.out.println("Valor Convertido: " + c+"ºCelsius");
+		JOptionPane.showMessageDialog(null, "Valor Convertido: " + fahrenheit(f) +"ºCelsius");
+		System.out.println("Valor Convertido: " + fahrenheit(f) +"ºCelsius");
 		
 		double k = Double.parseDouble(JOptionPane.showInputDialog(null,"\nConversor de Kelvin para Celsius\nDigite o valor de Kelvin: "));
-		b = k-273.15;
 		
-		JOptionPane.showMessageDialog(null, "Valor Convertido: " + df.format(b) + "ºkelvin");
-		System.out.println("Valor Convertido: " + b+"ºCelsius");
+		
+		JOptionPane.showMessageDialog(null, "Valor Convertido: " + df.format(kelvin(k)) + "ºkelvin");
+		System.out.println("Valor Convertido: " + df.format(kelvin(k))+"ºCelsius");
 	}
 
 }
